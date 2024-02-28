f17f327f8b6b7d38727bfbe18447345bad78253c
abdullah628 <abdullah628360@gmail.com>
2024-02-28 21:57:40 +0600

    project data update

--- lib/data.ts
+++ lib/data.ts
@@ -3,7 +3,7 @@ import { CgWorkAlt } from "react-icons/cg";
 import { FaReact } from "react-icons/fa";
 import { LuGraduationCap } from "react-icons/lu";
 import carHubImg from "@/public/carHub.png";
-import rmtdevImg from "@/public/rmtdev.png";
+import shoply from "@/public/shoply.png";
 import wordanalyticsImg from "@/public/wordanalytics.png";
 
 export const links = [
@@ -64,13 +64,13 @@ export const projectsData = [
     demoLiveUrl: "https://car-hub-sand-xi.vercel.app/"
   },
   {
-    title: "rmtDev",
+    title: "Shoply",
     description:
-      "Job board for remote developer jobs. I was the front-end developer. It has features like filtering, sorting and pagination.",
-    tags: ["React", "TypeScript", "Next.js", "Tailwind", "Redux"],
-    imageUrl: rmtdevImg,
-    codeUrl: "https://github.com/Abdullah628/",
-    demoLiveUrl: "https://github.com/Abdullah628/"
+      "Shoply e-commerce site. It's a simple online shop made with React and styled nicely with Tailwind CSS. Find cool stuff for your style",
+    tags: ["React", "JavaScript", "Tailwind"],
+    imageUrl: shoply,
+    codeUrl: "https://github.com/Abdullah628/shoply",
+    demoLiveUrl: "https://shoply-six.vercel.app/"
   },
   {
     title: "Word Analytics",
